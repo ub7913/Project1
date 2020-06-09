3072429c4ed196519cc036eadae20f785239b43d
kimpoly <kimpoly@localhost>
2020-06-10 00:31:34 +0900

    j

--- src/Pet/PetHospitalController.java
+++ src/Pet/PetHospitalController.java
@@ -1,5 +1,6 @@
 package Pet;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
@@ -8,6 +9,8 @@ public class PetHospitalController implements Initializable{
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		Calendar cal = Calendar.getInstance()
+				cal.
 		
 		
 		
