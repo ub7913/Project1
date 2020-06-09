7af61257b37e4d84084a50d5bd56c9ac2269e546
admin <admin@YD03-20>
2020-06-09 12:17:27 +0900

    2020-06-09

---
+++ src/Pet/PetHospitalController.java
@@ -0,0 +1,16 @@
+package Pet;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class PetHospitalController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		
+	}
+
+}
---
+++ src/PetHospitalController.java
@@ -0,0 +1,15 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class PetHospitalController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		
+	}
+
+}
